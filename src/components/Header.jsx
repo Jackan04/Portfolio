0c5188667f81d3fcc766a18fbda5dc6179936775
Jackan04 <jacob.asker@icloud.com>
2025-09-14 09:11:13 +0200

    -Applied styling for header -Added some breakpont rules for mobile devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,19 @@ export default function Header(){
 
     return(
         <header>
-            <h1 className="header-logo">Jacob</h1>
+            <Link to="/" className="header-logo">Jacob Asker</Link>
             
               <nav>
                 <Link to="/">Home</Link>
                 <Link to="/projects">Projects</Link>
                 </nav>
+                <div className='header-controls-mobile'>
+                    <button className='theme-button-mobile'>Theme</button>
+                    <button className="hamburger">☰</button>
+                </div>
 
             <div className="header-controls">
-                <button className='theme-button'>Toggle Theme</button>
+                <button className='theme-button'>Theme</button>
                 <a href="mailto: jacob.asker@icloud.com">Contact</a>
             </div>
             
