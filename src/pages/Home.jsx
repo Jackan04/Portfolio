f1f5bd79979d0afac72d042dceb97da8b94899d8
Jackan04 <jacob.asker@icloud.com>
2025-09-14 18:10:16 +0200

    -added first few elements to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import styles from '../components/Home/Home.module.css'
+import profile from '../assets/images/profile.jpeg'
+
 export default function Home(){
     
     return(
-        <>
-        </>
+        <main>
+            <div>
+            <img className={styles.profileImage} src={profile} alt="Profile Image" />
+            <h1 className={`${styles.heading} heading`}>Hi, I’m Jacob</h1>
+            <p>I’m an Informatics student at Högskolan Väst in Sweden, fascinated by consumer tech, software development, and design. Outside of tech, I’m into cars, strength training, photography, and creative side projects.</p>
+            </div>
+
+        </main>
+
+
     )
 }
