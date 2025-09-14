c1a6b8931c581e37c1335e3017c0f2880c55f9f8
Jackan04 <jacob.asker@icloud.com>
2025-09-14 13:17:53 +0200

    -Added a conditional sun icon for when dark mode is enabled -Added conditional header shadow depending on current theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,17 @@
 import { Link } from 'react-router-dom'
 import React, {useState} from 'react';
 import MoonIcon from '../assets/icons/moon.svg?react'
-
+import SunIcon from '../assets/icons/sun.svg?react'
 
 export default function Header(){
     const [theme, setTheme] = useState("light")
 
     function showDropdown(){
-        const dropdown = document.querySelector("#dropdown-menu")
-        dropdown.style.display = dropdown.style.display === "none" ? "flex" : "none";
+        const dropdown = document.querySelector('#dropdown-menu');
+        dropdown.classList.toggle('open');
     }
 
-    const toggleTheme = () => {
-            
+    function toggleTheme(){
         setTheme((prevTheme) => {
             const nextTheme = prevTheme === 'light' ? 'dark' : 'light';
             document.documentElement.classList.toggle('dark', nextTheme === 'dark');
@@ -22,28 +21,37 @@ export default function Header(){
     }
 
     return(
+
         <header>
             <Link to="/" className="header-logo">Jacob Asker</Link>
-            
-              <nav>
+
+            <nav>
                 <Link to="/">Home</Link>
                 <Link to="/projects">Projects</Link>
-                </nav>
-                <div className='header-controls-mobile'>
-                    <button className='theme-button-mobile' onClick={toggleTheme}><MoonIcon className="icon"></MoonIcon></button>
-                    <button className="hamburger" onClick={showDropdown}>☰</button>
-                    
-                    <div className='dropdown-menu' id='dropdown-menu'>
-                        <Link to="/">Home</Link>
-                        <Link to="/projects">Projects</Link>
-                        <a href="mailto: jacob.asker@icloud.com">Contact</a>
-                    </div>
-                </div>
+            </nav>
 
-                <div className="header-controls">
-                    <button className='theme-button' onClick={toggleTheme}><MoonIcon className="icon"></MoonIcon></button>
-                    <a href="mailto: jacob.asker@icloud.com">Contact</a>
-                </div>
+            <div className='header-controls-mobile'>
+
+            <button 
+                className='theme-button-mobile' 
+                onClick={toggleTheme}>
+                {theme === "light" ? <MoonIcon className="icon"></MoonIcon> : <SunIcon className="icon"></SunIcon>}
+            </button>
+
+            <button className="hamburger" onClick={showDropdown}>☰</button>
+
+            <div className='dropdown-menu' id='dropdown-menu'>
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+                <a href="mailto: jacob.asker@icloud.com">Contact</a>
+            </div>
+
+            </div>
+
+            <div className="header-controls">
+                <a href="mailto: jacob.asker@icloud.com">Contact</a>
+                <button className='theme-button' onClick={toggleTheme}><MoonIcon className="icon"></MoonIcon></button>
+            </div>
             
         </header>
     )
