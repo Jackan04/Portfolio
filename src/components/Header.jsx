a180632ccd35b3b1b232db22177eaf1afa919219
Jackan04 <jacob.asker@icloud.com>
2025-09-14 09:55:42 +0200

    - Implement dropdown menu component - Show and hide menu when hamburger button is clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,11 @@ import MoonIcon from '../assets/icons/moon.svg?react'
 
 export default function Header(){
 
+    function showDropdown(){
+        const dropdown = document.querySelector("#dropdown-menu")
+        dropdown.style.display = dropdown.style.display === "none" ? "flex" : "none";
+    }
+
     return(
         <header>
             <Link to="/" className="header-logo">Jacob Asker</Link>
@@ -13,13 +18,19 @@ export default function Header(){
                 </nav>
                 <div className='header-controls-mobile'>
                     <button className='theme-button-mobile'><MoonIcon></MoonIcon></button>
-                    <button className="hamburger">☰</button>
+                    <button className="hamburger" onClick={showDropdown}>☰</button>
+                    
+                    <div className='dropdown-menu' id='dropdown-menu'>
+                        <Link to="/">Home</Link>
+                        <Link to="/projects">Projects</Link>
+                        <a href="mailto: jacob.asker@icloud.com">Contact</a>
+                    </div>
                 </div>
 
-            <div className="header-controls">
-                <button className='theme-button'><MoonIcon></MoonIcon></button>
-                <a href="mailto: jacob.asker@icloud.com">Contact</a>
-            </div>
+                <div className="header-controls">
+                    <button className='theme-button'><MoonIcon></MoonIcon></button>
+                    <a href="mailto: jacob.asker@icloud.com">Contact</a>
+                </div>
             
         </header>
     )
