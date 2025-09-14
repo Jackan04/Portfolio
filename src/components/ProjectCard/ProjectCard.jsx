4240df1c7029ab9f36ba23d0035c88952255d93f
Jackan04 <jacob.asker@icloud.com>
2025-09-14 17:52:19 +0200

    Refactor styles: move header and project card styling to separate CSS modules for cleaner structure Simplify filter reset: remove unnecessary showAll function, now reset by passing null to filter function

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -5,7 +5,9 @@ export default function ProjectCard(props){
     return(
 
         <div className={styles.projectCard} category={props.category}>
-            <img src={props.src} alt={props.alt} />
+            <div className={styles.projectCardImageWrapper}>
+                <img src={props.src} alt={props.alt} />
+            </div>
             <div className={styles.projectCardContent}>
                     <h2 className={styles.projectCardTitle}><a href={props.href}>{props.title}</a></h2> 
                     <p className={styles.projectCardDescription}>{props.description}</p>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react"
 import ProjectCard from "../components/ProjectCard/ProjectCard"
+import styles from '../components/ProjectCard/ProjectCard.module.css'
 
 export default function Projects(){
-    const categories = ["coding", "design"]
+    const categories = ["Coding", "Design"]
     const [filter, setFilter] = useState(null);
 
     const allCards = [
         {
-            category: "coding",
+            category: "Coding",
             src: "https://culturedcode.com/things/2024-01-20/images/hero-logo-things-io70.png",
             href: "https://culturedcode.com/things/",
-            title: "My Project",
-            description: "A minimalist task manager built with React and Firebase. It focuses on clean design, fast performance, and simple collaboration, making everyday planning effortless on any device."
+            title: "Coding Project",
+            description: "A minimalist task manager built with React and Firebase. It focuses on clean Design, fast performance, and simple collaboration, making everyday planning effortless on any device."
             },
             {
-            category: "design",
-            src: "https://culturedcode.com/things/2024-01-20/images/hero-logo-things-io70.png",
+            category: "Design",
+            src: "https://m-cdn.phonearena.com/images/hub/550-wide-two_1200/iPhone-17-Pro-Max-release-date-price-and-features.jpg",
             href: "https://culturedcode.com/things/",
-            title: "My Project 2",
-            description: "A minimalist task manager built with React and Firebase. It focuses on clean design, fast performance, and simple collaboration, making everyday planning effortless on any device."
+            title: "Design Project",
+            description: "A minimalist task manager built with React and Firebase. It focuses on clean Design, fast performance, and simple collaboration, making everyday planning effortless on any device."
             },
     ]
        
@@ -26,21 +27,19 @@ export default function Projects(){
         setFilter(category);
     }
 
-    function showAllCards(){
-        setFilter(null)
-    }
-
     let visibleCards = filter ? allCards.filter(card => card.category === filter) : allCards;
 
     return(
     <main>
-        <div className="filter-buttons">
-        <button onClick={showAllCards}>All</button>
-        {categories.map(category => <button key={category} onClick={() => filterProjects(category)}>{category}</button>)}
+        <div className={styles.projectFilters}>
+        <button className={styles.filterButton} onClick={() => filterProjects(null)}>All</button>
+        {categories.map(category => <button className={styles.filterButton} key={category} onClick={() => filterProjects(category)}>{category}</button>)}
 
         </div>
         {/*{...card} is used to pass all the props from the allCards array, less manual writing within the jsx itself */}
-        {visibleCards.map(card =><ProjectCard key={card.title} {...card} />)}
+        <div className={styles.projectCards}>
+            {visibleCards.map(card =><ProjectCard key={card.title} {...card} />)}
+        </div>
     </main>
     )
 }
