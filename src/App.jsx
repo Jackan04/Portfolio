a3cd9d0c31dd8798ac1b198bb4886b659dc0274a
Jackan04 <jacob.asker@icloud.com>
2025-09-20 17:58:32 +0200

    fixed wrong naming for import and path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        {/* other routes */}
       </Routes>
     </HashRouter>
   );
