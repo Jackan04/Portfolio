1886bc3ba14e93809e046d12ac2d90a04447c1e9
Jackan04 <jacob.asker@icloud.com>
2025-11-05 06:16:45 +0100

    Updated theme toggle icons: replaced toggle-on/off SVGs with lightbulb icons and updated css styles and class names

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import React, {useState, useEffect} from 'react';
-import ToggleOnIcon from '../../assets/icons/toggle-on.svg?react'
-import ToggleOffIcon from '../../assets/icons/toggle-off.svg?react'
+import LightBulbIcon from '../../assets/icons/lightbulb.svg?react'
+import LightBulbFillIcon from '../../assets/icons/lightbulb-fill.svg?react'
 import HamburgerIcon from '../../assets/icons/hamburger.svg?react'
 
 
@@ -23,39 +23,66 @@ export default function Header(){
     }
     
 
-    return(
-        <header className={styles.header}>
-            <Link to="/" className={styles.headerLogo}>Jacob Asker</Link>
+    return (
+      <header className={styles.header}>
+        <Link to="/" className={styles.headerLogo}>
+          Jacob Asker
+        </Link>
 
-            <nav className={styles.nav}>
-                <Link to="/">Home</Link>
-                <Link to="/projects">Projects</Link>
-            </nav>
+        <nav className={styles.nav}>
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+        </nav>
 
-            <div className={styles.headerControlsMobile}>
-                <button className={styles.themeButtonMobile} onClick={toggleTheme}>
-                    {theme === "light" ? <ToggleOffIcon className={styles.icon} /> : <ToggleOnIcon className={`${styles.icon} ${styles.toggleOn}`} />}
-                </button>
+        <div className={styles.headerControlsMobile}>
+          <button className={styles.themeButtonMobile} onClick={toggleTheme}>
+            {theme === "light" ? (
+              <LightBulbFillIcon
+                className={`${styles.icon} ${styles.iconLightBulbFill}`}
+              />
+            ) : (
+              <LightBulbIcon className={`${styles.icon} `} />
+            )}
+          </button>
 
-                <button className={styles.hamburger} onClick={() => setDropdownOpen(open => !open)}>
-                    <HamburgerIcon className={styles.icon} />
-                </button>
+          <button
+            className={styles.hamburger}
+            onClick={() => setDropdownOpen((open) => !open)}
+          >
+            <HamburgerIcon className={styles.icon} />
+          </button>
 
-                <div className={`${styles.dropdownMenu} ${dropdownOpen ? 'open' : ''}`}>
-                    <Link to="/" onClick={() => setDropdownOpen(false)}>Home</Link>
-                    <Link to="/projects" onClick={() => setDropdownOpen(false)}>Projects</Link>
-                    <a href="mailto: jacob.asker@icloud.com" onClick={() => setDropdownOpen(false)}>Contact</a>
-                </div>
-            </div>
-                    
+          <div
+            className={`${styles.dropdownMenu} ${dropdownOpen ? "open" : ""}`}
+          >
+            <Link to="/" onClick={() => setDropdownOpen(false)}>
+              Home
+            </Link>
+            <Link to="/projects" onClick={() => setDropdownOpen(false)}>
+              Projects
+            </Link>
+            <a
+              href="mailto: jacob.asker@icloud.com"
+              onClick={() => setDropdownOpen(false)}
+            >
+              Contact
+            </a>
+          </div>
+        </div>
 
-            <div className={styles.headerControls}>
-                <a href="mailto: jacob.asker@icloud.com">Contact</a>
-                
-                <button className={styles.themeButton} onClick={toggleTheme}>
-                    {theme === "light" ? <ToggleOffIcon className={styles.icon} /> : <ToggleOnIcon className={`${styles.icon} ${styles.toggleOn}`} />}
-                </button>
-            </div>
-        </header>
-    )
+        <div className={styles.headerControls}>
+          <a href="mailto: jacob.asker@icloud.com">Contact</a>
+
+          <button className={styles.themeButton} onClick={toggleTheme}>
+            {theme === "light" ? (
+              <LightBulbFillIcon
+                className={`${styles.icon} ${styles.iconLightBulbFill}`}
+              />
+            ) : (
+              <LightBulbIcon className={`${styles.icon}`} />
+            )}
+          </button>
+        </div>
+      </header>
+    );
 }
